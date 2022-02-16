e74828467f732fabb7c473c23d9f2b489b20af69
Zvi Baratz <z.baratz@gmail.com>
2022-02-16 16:20:20 +0200

    Removed labels.

--- preview.py
+++ preview.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+
 import gl
 
-path = "<update>/nii/converted"
+path = Path("/path/to/here")
+nii = str(path / "nii/converted")
 
 gl.resetdefaults()
 gl.bmpzoom(1)
-gl.loadimage(path)
+gl.loadimage(nii)
 gl.extract(0, 1, 5)
 gl.mosaic(
-    "A L+ H -0.1 V -0.1 0.125 0.25 0.375 0.5; 0.625 0.75 0.875 S X R 0.5"
+    "A L- H -0.1 V -0.1 0.125 0.25 0.375 0.5; 0.625 0.75 0.875 S X R 0.5"
 )
-gl.savebmp("<update>/preview.png")
+gl.savebmp(str(path / "preview.png"))
